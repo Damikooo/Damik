642f6fea13d3e28df896656feda0486c76243fae
Damikooo <30871485+Damikooo@users.noreply.github.com>
2017-11-20 22:41:06 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1511201264,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/deniska-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
